37e7674680b080c26d8642c2018a3ef3bb29e754
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-20 00:58:30 +0530

    read with no flag

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,6 +4,6 @@ echo "Enter your PIN to login the server" # This will ask for value
 
 read secret # read will read the entered value and store it in secret which is a variable, but this will show the entered PIN value on the screen
 
-read -s secret # -s flag will hide the entered PIN value (like how the password is hidden for Linux server login)
+# read -s secret # -s flag will hide the entered PIN value (like how the password is hidden for Linux server login)
 
 echo "The PIN entered by you is $secret"
