932495ced4c25c6d059b78daaf27d70ad5e5cbd2
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-19 09:20:04 +0530

    conversation using basic variables

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Person1=Srini
+Person2=Nani
+
+echo "$Person1 - Hi $Person2, How are you ?"
+echo "$Person2 - Hi $Person1, I'm doing good, thanks for asking. How are you doing ?"
+echo "$Person1 - I'm doing fine $Person2. Thank you."
+echo "$Person2 - Whatsup $Person1?"
+echo "$Person1 - Shall we go to WonderLa this weekend ? we can a nice time"
+echo "$Person2 - Oh yeah! This is very exciting"
+echo "$Person1 - Ok, We will plan accordingly"
+echo "$Person2 - Cool......"
