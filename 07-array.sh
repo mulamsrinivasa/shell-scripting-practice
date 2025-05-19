0b19a75ad2a7753c51c3f7506f211337574b3602
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-20 01:34:55 +0530

    basic array

--- 07-array.sh
+++ 07-array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Heroes=("Mahesh Babu" "Jr.NTR" "Prabhas")
+Heroes=("Mahesh Babu" "Jr. NTR" "Prabhas")
 
-echo "First Hero = ${Heroes[0]}"
-echo "Second Hero = ${Heroes[1]}"
-echo "Third Hero = ${Heroes[2]}"
+echo "First Hero: ${Heroes[0]}"
+echo "Second Hero: ${Heroes[1]}"
+echo "Third Hero: ${Heroes[2]}"
