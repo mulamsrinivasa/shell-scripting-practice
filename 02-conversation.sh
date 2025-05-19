6f556b3e13b01caef14d1786e876f7de057399f3
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-19 09:08:56 +0530

    general conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Srini - Hi Nani, How are you ?"
+echo "Nani - Hi Srini, I'm doing good, thanks for asking. How are you doing ?"
+echo "Srini - I'm doing fine. Thank you."
+echo "Nani - Whatsup ?"
+echo "Srini - Shall we go to WonderLa this weekend ? we can a nice time"
+echo "Nani - Oh yeah! This is very exciting"
+echo "Srini - Ok, We will plan accordingly"
+echo "Nani - Cool......"
