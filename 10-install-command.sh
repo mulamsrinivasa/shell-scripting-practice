ad2b7629c560cbf349c0ee22ce1d3426ada0029d
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-21 08:48:50 +0530

    install a package with conditions

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,6 +5,7 @@ UserID=$(id -u)
 if [ $UserID -ne 0 ]
 then
     echo "SUGGESTION: You're a Normal user. Please install the package as root user"
+    exit 1
 else
     echo "Installing the package as root user"
 fi
