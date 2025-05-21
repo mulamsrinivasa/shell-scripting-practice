35ed969c0fe1bb792e103cc852a5f1fa6eeea4f0
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-21 09:05:42 +0530

    install a package with conditions

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 UserID=$(id -u)
+PKG="mysql"
 
 if [ $UserID -ne 0 ]
 then
     echo "SUGGESTION: You're a Normal user. Please install the package as root user"
     exit 1
 else
-    echo "Installing the package as root user"
+    echo "You're a root user, proceeding further"
 fi
 
-dnf install mysql -y
+echo "Checking if the $PKG package is installed or not"
+dnf list installed mysql
+
+if [ $? -eq 0 ]
+then
+    echo "The $PKG package is already installed... Nothing to do."
+else
+    echo "The $PKG package is not installed, proceeding to install it now."
+fi
+
+# dnf install mysql -y
