d56be76d08b7ed60a2de0c88fb20bb417732125b
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-21 09:54:58 +0530

    install a package with conditions

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -11,17 +11,17 @@ else
     echo "You're a root user, proceeding further"
 fi
 
-# echo "Checking if the $PKG package is installed or not"
-# dnf list installed mysql
+echo "Checking if the $PKG package is installed or not"
+dnf list installed mysql
 
-# if [ $? -eq 0 ]
-# then
-#     echo "The $PKG package is already installed... Nothing to do."
-#     exit 0
-# else
-#     echo "The $PKG package is not installed, proceeding to install it now."
-#     dnf install mysql -y
-#     echo "The $PKG package is now installed, good luck."
-# fi
+if [ $? -eq 0 ]
+then
+    echo "The $PKG package is already installed... Nothing to do."
+    exit 0
+else
+    echo "The $PKG package is not installed, proceeding to install it now."
+    dnf install mysql -y
+    echo "The $PKG package is now installed, good luck."
+fi
 
 # # dnf install mysql -y
