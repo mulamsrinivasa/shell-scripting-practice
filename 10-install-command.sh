14ea2f9b2ec934791280ce71bf8ebb7d72378558
SrinivasaKumarMulam <mulam.srinivasa@gmail.com>
2025-05-21 08:47:50 +0530

    install a package with conditions

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -6,7 +6,7 @@ if [ $UserID -ne 0 ]
 then
     echo "SUGGESTION: You're a Normal user. Please install the package as root user"
 else
-    echo "Installation the package as root user"
+    echo "Installing the package as root user"
 fi
 
-dnf install mysqld -y
+dnf install mysql -y
